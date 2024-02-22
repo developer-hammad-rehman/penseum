e3a4ce24768b9207d266ea5bbfcefe7fb574676f
Hammad-Rehman <124163466+developer-hammad-rehman@users.noreply.github.com>
2024-02-21 20:49:21 -0800

    quiz

---
+++ src/app/api/quiz/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
+export async function POST(req: NextRequest) {
+try {
+    const { prompt } = await req.json();
+    if (prompt) {
+    const openai = new OpenAI({
+      apiKey: process.env.OPEN_AI_API_KEY,
+    });
+    const chat = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages:[{role:"system" , content:`You Have to generate minimun 15 mcqs from this topic ${prompt} and also mark the correct question in it . if provided content is not ethical to generate quiz then give error that provide ethical content`}]
+    });
+    return NextResponse.json({message : chat.choices[0].message.content});
+    }
+    else{
+    return new NextResponse("Plese Enter Your prompt" , {status:404})
+    }
+} catch (error) {
+    return new NextResponse("Something Went Wrong" , {status:500})
+}
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 export default function Home() {
-  return (
-    <div className='h-screen w-full flex justify-center items-center flex-col'>
+return (
+    <div className='h-screen w-full flex justify-center items-center flex-col gap-4'>
         <h1 className='text-purple-300'>Api For Penseum</h1>
   <div className='text-2xl font-bold bg-slate-400 p-6'>
    <h2 className='text-green-200'>Chat Bot Api</h2>
    <span className='flex'>
     <div>POST : </div>
     https://penseum.vercel.app/api/chatbot
+    </span>
+  </div>
+  <div className='text-2xl font-bold bg-slate-400 p-6'>
+   <h2 className='text-green-200'>Generate Quiz</h2>
+   <span className='flex'>
+    <div>POST : </div>
+    https://penseum.vercel.app/api/quiz
     </span>
   </div>
    </div>
